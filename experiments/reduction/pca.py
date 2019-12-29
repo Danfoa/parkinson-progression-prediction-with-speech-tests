f4cfe3194ead4b4159f90e901408855fe50ec4a9
Albert Rial <albert.rial@est.fib.upc.edu>
2019-12-29 21:27:04 +0100

    PCA done with Fuzzy-C-Means

---
+++ experiments/reduction/pca.py
@@ -0,0 +1,45 @@
+import numpy
+import pandas
+from sklearn.preprocessing import MinMaxScaler
+
+from utils.dataset_loader import ParkinsonDataset
+from sklearn.decomposition import PCA
+
+
+def get_reduced_dataset(cluster_data, all_data):
+    pca = PCA(.95)
+    pca.fit_transform(cluster_data)
+    return pca.transform(all_data)
+
+
+if __name__ == '__main__':
+    load_path = "../../results/clustering/"
+    save_path = "../../results/reduction/"
+
+    clustering_algorithms = ["fuzzy_c_means"]
+
+
+    # loading the dataset
+    df = ParkinsonDataset.load_dataset(path="../dataset/parkinsons_updrs.data",
+                                       return_gender=False)
+
+    # Normalizing/scaling  dataset
+    feature_normalizers = ParkinsonDataset.normalize_dataset(dataset=df,
+                                                             scaler=MinMaxScaler(),
+                                                             inplace=True)
+
+    X = df[ParkinsonDataset.FEATURES].values
+
+    for algorithm in clustering_algorithms:
+        for c in [4, 5]:
+            labels = numpy.load(load_path + algorithm + '/C=%d-labels.npy' % c)
+            for k in range(c):
+                reduced_dataset = get_reduced_dataset(X[numpy.where(labels == k)], X)
+                numpy.save(save_path + algorithm + '/C=%d-K=%d-reduced-dataset.npy' % (c, k), reduced_dataset)
+
+
+
+
+
+
+
