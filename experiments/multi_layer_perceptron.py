30c1088be72bce25cdcf886f7b2cfadffc7ded05
Albert Rial <albert.rial@est.fib.upc.edu>
2020-01-06 13:48:25 +0100

    Modified MLP experiment to do 10-Fold cross-validation

--- experiments/multi_layer_perceptron.py
+++ experiments/multi_layer_perceptron.py
@@ -17,8 +17,8 @@ from sklearn.decomposition import PCA
 from utils.dataset_loader import ParkinsonDataset
 from utils.visualizer import *
 
-EXECUTION_MODE = "RUN"
-# EXECUTION_MODE = "SEARCH"
+# EXECUTION_MODE = "RUN"
+EXECUTION_MODE = "SEARCH"
 
 if __name__ == '__main__':
     model_name = "MLP"
@@ -51,10 +51,7 @@ if __name__ == '__main__':
     # ________________________________________________________________________________________________
 
     if EXECUTION_MODE == "SEARCH":
-        hidden_units = [[100, 50, 25], [500, 400, 300], [1000, 500, 250], [500, 250, 100],
-                        [50, 25, 10], [16, 50, 10], [2000, 1000, 500], [300, 300, 200, 50],
-                        [500, 250, 100, 50], [500, 400, 300, 200], [16, 100], [100],
-                        [400, 400, 400, 5], [300, 300, 300, 10]]
+        hidden_units = [[500, 400, 300, 200]]
 
         results = pandas.DataFrame(
             columns=['Total-Test', "Total-Params", 'Motor-Test', "Motor-Params"],
@@ -64,13 +61,13 @@ if __name__ == '__main__':
         for num_model in numpy.arange(len(hidden_units)):
             best_total = None
             best_motor = None
-            activations_perms = combinations_with_replacement(['relu', 'sigmoid'], len(hidden_units[num_model]))
+            activations_perms = ['sigmoid']
 
-            for activations in list(activations_perms):
-                for lr in [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]:
+            for activation in activations_perms:
+                for lr in [0.0005]:
                     model = keras.Sequential()
                     for layer in range(len(hidden_units[num_model])):
-                        model.add(layers.Dense(units=hidden_units[num_model][layer], activation=activations[layer]))
+                        model.add(layers.Dense(units=hidden_units[num_model][layer], activation=activation))
 
                     # 2 units in the output layer (Total and Motor)
                     model.add(layers.Dense(units=2))
@@ -78,6 +75,8 @@ if __name__ == '__main__':
                     model.compile(loss='mse',
                                   optimizer=optimizer,
                                   metrics=['mae', 'mse'])
+
+                    print(X_train.shape)
 
                     history = model.fit(x=X_train,
                                         y=y_train,
@@ -92,7 +91,7 @@ if __name__ == '__main__':
                     mae_total = mean_absolute_error(y_test[:, 0], y_pred[:, 0])
                     mae_motor = mean_absolute_error(y_test[:, 1], y_pred[:, 1])
 
-                    parameters = {'activations': activations, 'lr': lr, 'hidden_units': hidden_units[num_model]}
+                    parameters = {'activations': activation, 'lr': lr, 'hidden_units': hidden_units[num_model]}
 
                     print("mae_total:", mae_total, "- parameters:", parameters)
                     print("mae_motor:", mae_motor, "- parameters:", parameters)
@@ -108,29 +107,34 @@ if __name__ == '__main__':
                         results.at[num_model, 'Motor-Params'] = parameters
 
                     print(results)
-        results.to_csv("../results/outputs/%s/MAE-diff-models.csv" % model_name)
+        #results.to_csv("../results/outputs/%s/MAE-diff-models.csv" % model_name)
         print(results)
 
     hidden_units = [500, 400, 300, 200]
     activation = 'sigmoid'
-    model = keras.Sequential()
-    for layer in range(len(hidden_units)):
-        model.add(layers.Dense(units=hidden_units[layer], activation=activation))
-    # 2 units in the output layer (Total and Motor)
-    model.add(layers.Dense(units=2))
-    optimizer = tf.keras.optimizers.Adam(lr)
-    model.compile(loss='mse',
-                  optimizer=optimizer,
-                  metrics=['mae', 'mse'])
-
+    lr = 0.0005
 
     results = pandas.DataFrame(columns=['Total-Test', 'Motor-Test'],
-                               index=["All", "Male", "Female"])
+                               index=["All", "Males", "Females"])
+
     # Create CV loop, providing indexes of training and testing
     total_results, motor_results = [], []
-    cv_splitter = KFold(n_splits=5, shuffle=True)
+    cv_splitter = KFold(n_splits=10, shuffle=True)
 
+    # ALL
     for train_index, test_index in cv_splitter.split(X_all):
+        model = keras.Sequential()
+        for layer in range(len(hidden_units)):
+            model.add(layers.Dense(units=hidden_units[layer], activation=activation))
+        # 2 units in the output layer (Total and Motor)
+        model.add(layers.Dense(units=2))
+        optimizer = tf.keras.optimizers.Adam(lr)
+        model.compile(loss='mse',
+                      optimizer=optimizer,
+                      metrics=['mae', 'mse'])
+
+        print(X_all[train_index].shape)
+        print(X_all[test_index].shape)
         history = model.fit(x=X_all[train_index],
                             y=y_all[train_index],
                             epochs=1000,
@@ -149,24 +153,79 @@ if __name__ == '__main__':
     results.at["All", "Total-Test"] = total_results
     results.at["All", "Motor-Test"] = motor_results
     print(results)
-    results.to_csv("../results/outputs/%s/MAE-diff-models.csv" % model_name)
+
+    # MALE
+    X_males = df_males[ParkinsonDataset.FEATURES].values
+    y_males = df_males[[ParkinsonDataset.TOTAL_UPDRS, ParkinsonDataset.MOTOR_UPDRS]].values
+    total_results, motor_results = [], []
+    for train_index, test_index in cv_splitter.split(X_males):
+        model = keras.Sequential()
+        for layer in range(len(hidden_units)):
+            model.add(layers.Dense(units=hidden_units[layer], activation=activation))
+        # 2 units in the output layer (Total and Motor)
+        model.add(layers.Dense(units=2))
+        optimizer = tf.keras.optimizers.Adam(lr)
+        model.compile(loss='mse',
+                      optimizer=optimizer,
+                      metrics=['mae', 'mse'])
+
+        print(X_males[train_index].shape)
+        print(X_males[test_index].shape)
+        history = model.fit(x=X_males[train_index],
+                            y=y_males[train_index],
+                            epochs=1000,
+                            validation_split=0.1,
+                            shuffle=True,
+                            verbose=0,
+                            callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)])
+
+        y_pred = model.predict(X_males[test_index])
+
+        mae_total = mean_absolute_error(y_males[test_index, 0], y_pred[:, 0])
+        mae_motor = mean_absolute_error(y_males[test_index, 1], y_pred[:, 1])
+        total_results.append(mae_total)
+        motor_results.append(mae_motor)
+
+    results.at["Males", "Total-Test"] = total_results
+    results.at["Males", "Motor-Test"] = motor_results
+    print(results)
 
 
-    rfecv = RFECV(estimator=model, step=1, cv=cv_splitter, scoring='neg_mean_absolute_error', n_jobs=-1, verbose=0)
-    for y_target, y_type in zip([y_total, y_motor], ['Total', 'Motor']):
-        rfecv.fit(X, y_target)
-        feature_masks[y_type] = rfecv.support_
-        mae_log[y_type] = rfecv.grid_scores_ * -1
+    # FEMALE
+    X_females = df_females[ParkinsonDataset.FEATURES].values
+    y_females = df_females[[ParkinsonDataset.TOTAL_UPDRS, ParkinsonDataset.MOTOR_UPDRS]].values
+    total_results, motor_results = [], []
 
-    history = model.fit(x=X_train,
-                        y=y_train,
-                        epochs=1000,
-                        validation_split=0.1,
-                        shuffle=True,
-                        verbose=0,
-                        callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)])
+    for train_index, test_index in cv_splitter.split(X_females):
+        model = keras.Sequential()
+        for layer in range(len(hidden_units)):
+            model.add(layers.Dense(units=hidden_units[layer], activation=activation))
+        # 2 units in the output layer (Total and Motor)
+        model.add(layers.Dense(units=2))
+        optimizer = tf.keras.optimizers.Adam(lr)
+        model.compile(loss='mse',
+                      optimizer=optimizer,
+                      metrics=['mae', 'mse'])
 
-    y_pred = model.predict(X_test)
+        print(X_females[train_index].shape)
+        print(X_females[test_index].shape)
+        history = model.fit(x=X_females[train_index],
+                            y=y_females[train_index],
+                            epochs=1000,
+                            validation_split=0.1,
+                            shuffle=True,
+                            verbose=0,
+                            callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)])
 
-    mae_total = mean_absolute_error(y_test[:, 0], y_pred[:, 0])
-    mae_motor = mean_absolute_error(y_test[:, 1], y_pred[:, 1])
+        y_pred = model.predict(X_females[test_index])
+
+        mae_total = mean_absolute_error(y_females[test_index, 0], y_pred[:, 0])
+        mae_motor = mean_absolute_error(y_females[test_index, 1], y_pred[:, 1])
+        total_results.append(mae_total)
+        motor_results.append(mae_motor)
+
+    results.at["Females", "Total-Test"] = total_results
+    results.at["Females", "Motor-Test"] = motor_results
+    print(results)
+
+    results.to_csv("../results/outputs/%s/MAE-final-MLP-KFold.csv" % model_name)
